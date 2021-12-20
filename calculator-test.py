233ad8f3e9dc990580a512271ce51576911cf8e1
Remigiusz Kozdra <rkozdra@gmail.com>
2021-12-20 23:00:13 +0100

    added tests

--- calculator-test.py
+++ calculator-test.py
@@ -18,44 +18,251 @@ class Initial_tests(unittest.TestCase):
 
     def test_initial_numeric_system(self):
         calc = Calculator()
-        self.assertEqual(calc.numericSystem, NumericSystem.bin)
+        self.assertEqual(calc.numericSystem, NumericSystem.dec)
 
     def test_initial_binary_value(self):
         calc = Calculator()
         self.assertEqual(calc.binaryValue, '0' * 64)
-
-    def test_initial_hex_value(self):
-        calc = Calculator()
-        self.assertEqual(calc.hexValue, '0')
-
-    def test_initial_oct_value(self):
-        calc = Calculator()
-        self.assertEqual(calc.octValue, '0')
 
 
 class Binary_input_tests(unittest.TestCase):
 
     def test1_when_input_1(self):
         calc = Calculator()
+        calc.numericSystem = NumericSystem.bin
+
         calc.input('1')
         self.assertEqual(calc.value, '1')
+        self.assertEqual(calc.operation, '')
 
     def test2_when_input_0(self):
         calc = Calculator()
+        calc.numericSystem = NumericSystem.bin
+
         calc.input('0')
         self.assertEqual(calc.value, '0')
+        self.assertEqual(calc.operation, '')
 
     def test2_when_input_other_numbers(self):
         calc = Calculator()
-        for number in ["234567890"]:
+        calc.numericSystem = NumericSystem.bin
+
+        for number in ["23456789"]:
             calc.input(number)
             self.assertEqual(calc.value, '0')
+            self.assertEqual(calc.operation, '')
 
     def test2_when_input_other_characters(self):
         calc = Calculator()
+        calc.numericSystem = NumericSystem.bin
+
         for letter in ["abcdefghijklmnoprstquvwzABCDEFGHIJKLMNOPRSTQUVWZ"]:
             calc.input(letter)
             self.assertEqual(calc.value, '0')
+            self.assertEqual(calc.operation, '')
+
+    def test_when_input_percent(self):
+        calc = Calculator()
+        calc.numericSystem = NumericSystem.bin
+
+        calc.input('%')
+        self.assertEqual(calc.value, '0')
+        self.assertEqual(calc.operation, '')
+
+    def test_when_input_plus_sign(self):
+        calc = Calculator()
+        calc.numericSystem = NumericSystem.bin
+        calc.input('+')
+        self.assertEqual(calc.operation, '+')
+        self.assertEqual(calc.value, '0')
+
+    def test_when_input_minus_sign(self):
+        calc = Calculator()
+        calc.numericSystem = NumericSystem.bin
+        calc.input('-')
+        self.assertEqual(calc.operation, '-')
+        self.assertEqual(calc.value, '0')
+
+    def test_when_input_slash_sign(self):
+        calc = Calculator()
+        calc.numericSystem = NumericSystem.bin
+        calc.input('/')
+        self.assertEqual(calc.operation, '/')
+        self.assertEqual(calc.value, '0')
+
+    def test_when_input_start_sign(self):
+        calc = Calculator()
+        calc.numericSystem = NumericSystem.bin
+        calc.input('*')
+        self.assertEqual(calc.operation, '*')
+        self.assertEqual(calc.value, '0')
+
+    def test_when_input_exclamation_sign(self):
+        calc = Calculator()
+        calc.numericSystem = NumericSystem.bin
+        calc.input('!')
+        self.assertEqual(calc.operation, '!')
+        self.assertEqual(calc.value, '0')
+
+    def test_when_input_equal_sign(self):
+        calc = Calculator()
+        calc.numericSystem = NumericSystem.bin
+        calc.input('=')
+        self.assertEqual(calc.operation, '=')
+        self.assertEqual(calc.value, '0')
+
+    def test_when_input_open_paren_sign(self):
+        calc = Calculator()
+        calc.numericSystem = NumericSystem.bin
+        calc.input('(')
+        self.assertEqual(calc.operation, '(')
+        self.assertEqual(calc.value, '0')
+
+    def test_when_input_close_paren_sign(self):
+        calc = Calculator()
+        calc.numericSystem = NumericSystem.bin
+        calc.input(')')
+        self.assertEqual(calc.operation, ')')
+        self.assertEqual(calc.value, '0')
+
+    def test_for_other_signs(self):
+        calc = Calculator()
+        calc.numericSystem = NumericSystem.bin
+
+        for i in range(12, 256):
+            if chr(i) in "01+-*/=!()":
+                continue
+            else:
+                # reset class to default value
+                calc.value = '0'
+                calc.operation = ''
+                # input char
+                calc.input(chr(i))
+                self.assertEqual(calc.operation, '')
+                self.assertEqual(calc.value, '0')
+
+
+class Oct_input_tests(unittest.TestCase):
+
+    def test_when_input_accepted_numeric_values(self):
+        calc = Calculator()
+        calc.numericSystem = NumericSystem.oct
+        for i in "12345670":
+            calc.value = '0'
+            calc.operation = ''
+
+            calc.input(i)
+            self.assertEqual(calc.value, i)
+            self.assertEqual(calc.operation, '')
+
+    def test_when_input_accepted_signs(self):
+        calc = Calculator()
+        calc.numericSystem = NumericSystem.oct
+        for i in "+-*/=!()":
+            calc.value = '0'
+            calc.operation = ''
+
+            calc.input(i)
+            self.assertEqual(calc.value, '0')
+            self.assertEqual(calc.operation, i)
+
+    def test_for_other_signs(self):
+        calc = Calculator()
+        calc.numericSystem = NumericSystem.oct
+
+        for i in range(12, 256):
+            if chr(i) in "01234567+-*/=!()":
+                continue
+            else:
+                # reset class to default value
+                calc.value = '0'
+                calc.operation = ''
+                # input char
+                calc.input(chr(i))
+                self.assertEqual(calc.operation, '')
+                self.assertEqual(calc.value, '0')
+
+
+class Dec_input_tests(unittest.TestCase):
+
+    def test_when_input_accepted_numeric_values(self):
+        calc = Calculator()
+        calc.numericSystem = NumericSystem.dec
+        for i in "1234567890":
+            calc.value = '0'
+            calc.operation = ''
+
+            calc.input(i)
+            self.assertEqual(calc.value, i)
+            self.assertEqual(calc.operation, '')
+
+    def test_when_input_accepted_signs(self):
+        calc = Calculator()
+        calc.numericSystem = NumericSystem.dec
+        for i in "+-*/=!()":
+            calc.value = '0'
+            calc.operation = ''
+
+            calc.input(i)
+            self.assertEqual(calc.value, '0')
+            self.assertEqual(calc.operation, i)
+
+    def test_for_other_signs(self):
+        calc = Calculator()
+        calc.numericSystem = NumericSystem.dec
+
+        for i in range(12, 256):
+            if chr(i) in "01234567890+-*/=!()":
+                continue
+            else:
+                # reset class to default value
+                calc.value = '0'
+                calc.operation = ''
+                # input char
+                calc.input(chr(i))
+                self.assertEqual(calc.operation, '')
+                self.assertEqual(calc.value, '0')
+
+
+class hex_input_tests(unittest.TestCase):
+
+    def test_when_input_accepted_numeric_values(self):
+        calc = Calculator()
+        calc.numericSystem = NumericSystem.hex
+        for i in "1234567890ABCDEF":
+            calc.value = '0'
+            calc.operation = ''
+
+            calc.input(i)
+            self.assertEqual(calc.value, i)
+            self.assertEqual(calc.operation, '')
+
+    def test_when_input_accepted_signs(self):
+        calc = Calculator()
+        calc.numericSystem = NumericSystem.hex
+        for i in "+-*/=!()":
+            calc.value = '0'
+            calc.operation = ''
+
+            calc.input(i)
+            self.assertEqual(calc.value, '0')
+            self.assertEqual(calc.operation, i)
+
+    def test_for_other_signs(self):
+        calc = Calculator()
+        calc.numericSystem = NumericSystem.hex
+
+        for i in range(12, 256):
+            if chr(i) in "01234567890ABCDEF+-*/=!()":
+                continue
+            else:
+                # reset class to default value
+                calc.value = '0'
+                calc.operation = ''
+                # input char
+                calc.input(chr(i))
+                self.assertEqual(calc.operation, '')
+                self.assertEqual(calc.value, '0')
 
 
 class Casting_tests(unittest.TestCase):
@@ -99,7 +306,7 @@ class Casting_tests(unittest.TestCase):
         calc.numericSystem = NumericSystem.bin
         # THEN
         self.assertEqual(
-            calc.value, '0000000000000000000000000000000000000000000000000000000000001101')
+            calc.value, '1101')
 
 
 unittest.main()
